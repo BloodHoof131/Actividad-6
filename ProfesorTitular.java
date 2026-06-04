64b2b0af1630e9268d800d0dddbd0c7e934f48a8
BloodHoof131 <blaverde@unal.edu.co>
2026-06-04 14:48:56 -0500

    correccion codigo

--- ProfesorTitular.java
+++ ProfesorTitular.java
@@ -1,14 +1,9 @@
 package Profesores;
-/**
-* Esta clase denominada ProfesorTitular es una subclase de Profesor
-* @version 1.2/2020
-*/
+
 public class ProfesorTitular extends Profesor {
-/**
-* Método que sobreescribe el método imprimir heredado de la clase 
-* padre Profesor
-*/
-protected void imprimir() {
-System.out.println(“Es un profesor titular.”);
-}
+
+    @Override
+    protected void imprimir() {
+        System.out.println("Es un profesor titular.");
+    }
 }
