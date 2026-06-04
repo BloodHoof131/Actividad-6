446baf96cefd23cbc8757f9fec96b2784bc139a0
BloodHoof131 <blaverde@unal.edu.co>
2026-06-04 15:46:25 -0500

    correcciones

--- Ejercicio 4.6/Profesores/Prueba3.java
+++ Ejercicio 4.6/Profesores/Prueba3.java
@@ -4,7 +4,7 @@ public class Prueba3 {
 
     public static void main(String[] args) {
 
-        Profesor profesor1 = new ProfesorTitular();
+        ProfesorTitular profesor1 = new ProfesorTitular();
         profesor1.imprimirAnios();
     }
 }
