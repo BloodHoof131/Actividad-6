6d9f7f8d28b92be5b6b3a4d569cefaae22f027b8
BloodHoof131 <blaverde@unal.edu.co>
2026-06-04 15:07:52 -0500

    correccion codigo

--- Gato.java
+++ Gato.java
@@ -1,36 +1,24 @@
 package Animales;
-/**
-* Esta clase concreta denominada Gato es una subclase de Felino.
-* @version 1.2/2020
-*/
+
 public class Gato extends Felino {
-/**
-* Método que devuelve un String con el sonido de un gato
-* @return Un valor String con el sonido de un gato: “Maullido”
-*/
-public String getSonido() {
-return “Maullido”;
-}
-/**
-* Método que devuelve un String con los alimentos de un gato
-* @return Un valor String con la alimentación de un gato: “Ratones”
-*/
-public String getAlimentos() {
-return “Ratones”;
-}
-/**
-* Método que devuelve un String con el hábitat de un gato
-* @return Un valor String con el hábitat de un gato: “Doméstico”
-*/
-public String getHábitat() {
-return “Doméstico”;
-}
-/**
-* Método que devuelve un String con el nombre científico de un gato
-* @return Un valor String con el nombre científico de un gato: 
-* “Felis silvestris catus”
-*/
-public String getNombreCientífico() {
-return “Felis silvestris catus”;
-}
+
+    @Override
+    public String getSonido() {
+        return "Maullido";
+    }
+
+    @Override
+    public String getAlimentos() {
+        return "Ratones";
+    }
+
+    @Override
+    public String getHabitat() {
+        return "Doméstico";
+    }
+
+    @Override
+    public String getNombreCientifico() {
+        return "Felis silvestris catus";
+    }
 }
