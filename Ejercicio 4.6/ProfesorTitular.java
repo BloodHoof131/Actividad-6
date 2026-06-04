7d3cb05966a586067ad59dd88057e185a6904526
BloodHoof131 <blaverde@unal.edu.co>
2026-06-04 14:53:28 -0500

    correccion codigo

--- Ejercicio 4.6/ProfesorTitular.java
+++ Ejercicio 4.6/ProfesorTitular.java
@@ -1,24 +1,15 @@
 package Profesores;
-/**
-* Esta clase denominada ProfesorTitular es una subclase de Profesor
-* @version 1.2/2020
-*/
+
 public class ProfesorTitular extends Profesor {
-/* Atributo que identifica la cantidad de años que el profesor ha sido 
-titular */
-int años = 0;
-/**
-* Método que sobreescribe el método imprimir heredado de la clase 
-* padre Profesor
-*/
-protected void imprimir() {
-System.out.println(“Es un profesor titular.”);
-}
-/**
-* Método que imprime en pantalla la cantidad de años que tiene un 
-* profesor siendo titular
-*/
-protected void imprimirAños() {
-System.out.println(“Años = “ + años);
-}
+
+    int anios = 0;
+
+    @Override
+    protected void imprimir() {
+        System.out.println("Es un profesor titular.");
+    }
+
+    protected void imprimirAnios() {
+        System.out.println("Años = " + anios);
+    }
 }
