9009b9150c386fa872b81fd4b2b4f03662f2ec21
BloodHoof131 <blaverde@unal.edu.co>
2026-06-04 14:46:57 -0500

    prueba creada

---
+++ ProfesorTitular.java
@@ -0,0 +1,14 @@
+package Profesores;
+/**
+* Esta clase denominada ProfesorTitular es una subclase de Profesor
+* @version 1.2/2020
+*/
+public class ProfesorTitular extends Profesor {
+/**
+* Método que sobreescribe el método imprimir heredado de la clase 
+* padre Profesor
+*/
+protected void imprimir() {
+System.out.println(“Es un profesor titular.”);
+}
+}
